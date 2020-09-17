adbb176435eb808c6c509bd7ef0eaa00c2140637
zeeshan9821 <61489241+zeeshan9821@users.noreply.github.com>
2020-09-17 08:38:46 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = ({cartToggle}) => {
     return(
         <nav>
  <ul className="left">
- <li><Link to="/">ALI BABA STORE</Link></li>
+ <li><Link to="/">ALI BABA </Link></li>
  {/* <li><form>
      <input type="text" className="search" placeholder="Search Product..."/>
      </form></li> */}
